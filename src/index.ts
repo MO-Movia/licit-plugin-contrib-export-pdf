09ccc7166eb78607efd5a933723d7bfbbdccc03e
Ashfaq Shamsudeen <ashfaq.shamsudeen@firmusoft.co.in>
2022-09-28 18:45:18 +0530

    - FIX: Keyboard shortcut not working.

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,23 @@
-import { Schema } from 'prosemirror-model';
-import { Plugin, PluginKey } from 'prosemirror-state';
+import {Schema} from 'prosemirror-model';
+import {Plugin, PluginKey} from 'prosemirror-state';
 import {
   makeKeyMapWithCommon,
   createKeyMapPlugin,
 } from '@modusoperandi/licit-doc-attrs-step';
 
-import {
-  ExportPDFCommand
-} from './exportPdfCommand';
+import {ExportPDFCommand} from './exportPdfCommand';
 
-export const KEY_EXPORT_PDF = makeKeyMapWithCommon('exportPDF', 'Mod-Alt' + '-p');
+export const KEY_EXPORT_PDF = makeKeyMapWithCommon(
+  'exportPDF',
+  'Mod-Alt' + '-p'
+);
 const EXPORT_PDF = new ExportPDFCommand();
-
 
 export class ExportPDFPlugin extends Plugin {
   constructor() {
     super({
       key: new PluginKey('exportPDF'),
-    },
-    );
+    });
   }
 
   // Plugin method that supplies plugin schema to editor
@@ -27,9 +26,12 @@ export class ExportPDFPlugin extends Plugin {
   }
 
   initKeyCommands(): unknown {
-    return createKeyMapPlugin({
-      [KEY_EXPORT_PDF.common]: EXPORT_PDF,
-    }, 'ExportPDFKeyMap');
+    return createKeyMapPlugin(
+      {
+        [KEY_EXPORT_PDF.common]: EXPORT_PDF.execute,
+      },
+      'ExportPDFKeyMap'
+    );
   }
 
   initButtonCommands(): unknown {
