43eaa77c141731655e81c9b808e7517a88b72a41
jayakrishnan <jayakrishnan.ms@firmusoft.co.in>
2023-09-25 15:55:48 +0530

    fix in unit test errors in ESM module

--- src/exportPdfCommand.test.ts
+++ src/exportPdfCommand.test.ts
@@ -1,14 +1,9 @@
 import {ExportPDFCommand} from './exportPdfCommand';
-import {EditorState} from 'prosemirror-state';
-import {EditorView} from 'prosemirror-view';
 
 describe('Export PDF Command', () => {
- 
-
   it('isEnabled returns true', () => {
     const exportPDFCommand = new ExportPDFCommand();
     const result = exportPDFCommand.isEnabled();
     expect(result).toBe(true);
   });
-
 });
