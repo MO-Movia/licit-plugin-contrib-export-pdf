f922dcd5286888e41b751a1a75f495095cae830a
rohan.john <rohan.john@firmusoft.co.in>
2023-11-09 16:12:00 +0530

    UICommand chnaged to abstract class.

--- src/exportPdfCommand.ts
+++ src/exportPdfCommand.ts
@@ -3,6 +3,7 @@ import { Transform } from 'prosemirror-transform';
 import { EditorView } from 'prosemirror-view';
 import { UICommand } from '@modusoperandi/licit-doc-attrs-step';
 import { ExportPDF } from './exportPdf';
+import * as React from 'react';
 
 export class ExportPDFCommand extends UICommand {
     exportPdf: ExportPDF;
@@ -15,6 +16,28 @@ export class ExportPDFCommand extends UICommand {
     isEnabled = (): boolean => {
         return true;
     };
+
+    waitForUserInput = (
+        _state: EditorState,
+        _dispatch?: (tr: Transform) => void,
+        _view?: EditorView,
+        _event?: React.SyntheticEvent
+      ): Promise<undefined> => {
+        return Promise.resolve(undefined);
+      };
+    
+      executeWithUserInput = (
+        _state: EditorState,
+        _dispatch?: (tr: Transform) => void,
+        _view?: EditorView,
+        _inputs?: string
+      ): boolean => {
+        return false;
+      };
+    
+      cancel(): void {
+        return null;
+      }
 
     execute = (
         _state: EditorState,
