36ddb6504e52fcc19b74d2280e4daf4f9c7e9aa5
Tyler <tnewton@modusoperandi.com>
2024-12-31 11:20:46 -0500

    [AT-477] - initial cleanup of component layout and styling

--- src/loader.tsx
+++ src/loader.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 export class Loader extends React.PureComponent<any, any> {
-  render(): React.ReactElement<any> {
+  render(): React.ReactNode {
     return (
       <div className="epdf-loader-container">
         <div className="epdf-loader"></div>
